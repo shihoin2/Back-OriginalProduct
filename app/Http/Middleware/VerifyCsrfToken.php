057762fc5aa0d1d3d14bd245a867c1c28e32b3a6
shihoin2 <merysousi@gmail.comm>
2024-07-13 08:50:23 +0900

    api.php csrf_torkenの変更

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -6,7 +6,6 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
 {
-
     /**
      * The URIs that should be excluded from CSRF verification.
      *
@@ -15,7 +14,6 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
 
         //
-        'api/*', // APIエンドポイント
-        'test/*', // テスト用エンドポイント
+
     ];
 }
